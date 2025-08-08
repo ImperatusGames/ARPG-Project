f2aead87f688413883473826d2bf1c43b6b9f01b
Imperatus <eric.starcore@gmail.com>
2025-08-08 19:18:14 -0400

    Small Cleanup
    -Removed Animation Tree
    -Slight code cleanup

--- Player/hero.gd
+++ Player/hero.gd
@@ -2,7 +2,6 @@ class_name Player extends Entity
 
 @onready var sprite_player = $AnimationPlayer
 @onready var sprite = $Sprite2D
-@onready var state_machine = $AnimationTree["parameters/playback"]
 var collider
 var last_direction : String
 var is_attacking : bool
@@ -28,66 +27,29 @@ func _ready() -> void:
 	last_direction = "right"
 	
 func _physics_process(_delta: float) -> void:
-	#var current = state_machine.get_current_node()
 	if can_move == true:
 		var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 		velocity = direction * velocity_component.current_speed
 		
-		#if direction.x > 0:
-			#state_machine.travel("walk_right")
-			#last_direction = "right"
-			#sprite.flip_h = false
-		#elif direction.x < 0:
-			#state_machine.travel("walk_right")
-			#last_direction = "left"
-			#sprite.flip_h = true
-		#elif direction.y > 0:
-			#state_machine.travel("walk_down")
-			#last_direction = "down"
-		#elif direction.y < 0:
-			#state_machine.travel("walk_up")
-			#last_direction = "up"
-		#if velocity.x > 0:
-			#state_machine.travel("walk_right")
-			#print("Walking right")
-		#else:
-			#state_machine.travel("idle")
-
-		
-	if Input.is_action_just_pressed("attack") && can_attack == true:
-		#print("Attack button pressed!")
-		#Need to add an Attack animation and, while it's playing, set player Velocity to 0
-		is_attacking = true
-		can_move = false
-		can_cast = false
-		can_attack = false
-		sword_attack(last_direction)
-		is_attacking = false
-		can_move = true
-		can_cast = true
-		can_attack = true
-
-	if Input.is_action_just_pressed("special") && can_cast == true:
-		#print("Special button pressed!")
-		#Need to add a Casting animation and, while it's playing, set player Velocity to 0
-		#heal()
-		is_casting = true
-		can_move = false
-		can_attack = false
-		can_cast = false
-		fireball(last_direction)
-		is_casting = false
-		can_move = true
-		can_attack = true
-		can_cast = true
-		#defender()
-
-	if Input.is_action_just_pressed("menu"):
-		print("Menu button pressed!")
+		if direction.x > 0:
+			sprite_player.play("walk_right")
+			last_direction = "right"
+			sprite.flip_h = false
+		elif direction.x < 0:
+			sprite_player.play("walk_right")
+			last_direction = "left"
+			sprite.flip_h = true
+		elif direction.y > 0:
+			sprite_player.play("walk_down")
+			last_direction = "down"
+		elif direction.y < 0:
+			sprite_player.play("walk_up")
+			last_direction = "up"
+		else:
+			sprite_player.play("idle")
 	
-	move_and_slide()
+		move_and_slide()
 	
-
 	#Add state machine for state control between each action
 
 func fireball(player_direction: String):
@@ -106,7 +68,6 @@ func fireball(player_direction: String):
 	#elif player_direction == "right":
 		#new_fireball.global_position.x += 60
 	
-	#new_fireball.collision_mask = 5
 	new_fireball.set_collision_mask_value(5, true)
 	new_fireball.set_collision_layer_value(4, true)
 	add_child(new_fireball)
@@ -129,75 +90,37 @@ func sword_attack(last_dir: String):
 	add_child(new_sword)
 
 func _input(event):
-	var direction = Input.get_vector("move_down", "move_left", "move_right", "move_up")
-	if event.is_action_pressed("move_down"):
-		pass
-	elif event.is_action_pressed("move_up"):
-		pass
-	elif event.is_action_pressed("move_left"):
-		pass
-	elif event.is_action_pressed("move_right"):
-		pass
-	elif event.is_action_pressed("attack"):
-		pass
-	elif event.is_action_pressed("special"):
-		pass
-	elif event.is_action_pressed("menu"):
-		pass
-	
-	var current = state_machine.get_current_node()
-	if can_move == true:
-		direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		velocity = direction * velocity_component.current_speed
-		
-		if direction.x > 0:
-			#state_machine.travel("walk_right")
-			last_direction = "right"
-			sprite.flip_h = false
-		elif direction.x < 0:
-			#state_machine.travel("walk_right")
-			last_direction = "left"
-			sprite.flip_h = true
-		elif direction.y > 0:
-			#state_machine.travel("walk_down")
-			last_direction = "down"
-		elif direction.y < 0:
-			#state_machine.travel("walk_up")
-			last_direction = "up"
-		#else:
-			#state_machine.travel("idle")
-
-		
-	if Input.is_action_just_pressed("attack") && can_attack == true:
-		#print("Attack button pressed!")
+	#var direction = Input.get_vector("move_down", "move_left", "move_right", "move_up")
+	#if event.is_action_pressed("move_down"):
+		#pass
+	#elif event.is_action_pressed("move_up"):
+		#pass
+	#elif event.is_action_pressed("move_left"):
+		#pass
+	#elif event.is_action_pressed("move_right"):
+		#pass
+	if event.is_action_pressed("attack"):
 		is_attacking = true
 		can_move = false
 		can_cast = false
 		can_attack = false
-		sword_attack(last_direction)
+		await sword_attack(last_direction)
 		is_attacking = false
 		can_move = true
 		can_cast = true
 		can_attack = true
-
-	if Input.is_action_just_pressed("special") && can_cast == true:
-		#print("Special button pressed!")
-		#heal()
+	elif event.is_action_pressed("special"):
 		is_casting = true
 		can_move = false
 		can_attack = false
 		can_cast = false
-		fireball(last_direction)
+		await fireball(last_direction)
 		is_casting = false
 		can_move = true
 		can_attack = true
 		can_cast = true
-		#defender()
-
-	if Input.is_action_just_pressed("menu"):
+	elif event.is_action_pressed("menu"):
 		print("Menu button pressed!")
-	
-	move_and_slide()
 		
 	
 #func heal():
