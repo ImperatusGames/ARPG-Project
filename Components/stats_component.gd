4487e43a96a2e24f17856026fcdd015c67aa67a2
Imperatus <eric.starcore@gmail.com>
2025-08-29 19:19:44 -0400

    Stats Structure Update
    -Partially sets up Stat formulas

--- Components/stats_component.gd
+++ Components/stats_component.gd
@@ -19,6 +19,17 @@ class_name StatsComponent
 @export var max_mp : int
 @export var current_mp : int
 
+var defense_bonus := 1.0
+var strength_bonus := 1.0
+var magic_bonus := 1.0
+var magic_def_bonus := 1.0
+
+var defense_penalty := 1.0
+var strength_penalty := 1.0
+var magic_penalty := 1.0
+var magic_def_penalty := 1.0
+
+
 func _ready() -> void:
 	current_defense = base_defense
 	current_strength = base_strength
@@ -26,6 +37,49 @@ func _ready() -> void:
 	current_magic_def = base_magic_def
 	current_mp = max_mp
 
+func set_defense():
+	current_defense = (base_defense * defense_bonus) * defense_penalty
+
+func set_strength():
+	current_strength = (base_strength * strength_bonus) * strength_penalty
+
+func set_magic():
+	current_magic = (base_magic * magic_bonus) * magic_penalty
+
+func set_magic_def():
+	current_magic_def = (base_magic_def * magic_def_bonus) * magic_def_penalty
+
+func set_defense_bonus_on():
+	defense_bonus = 1.5
+	set_defense()
+
+func set_strength_bonus_on():
+	strength_bonus = 1.5
+	set_strength()
+
+func set_magic_bonus_on():
+	magic_bonus = 1.5
+	set_magic()
+
+func set_magic_def_bonus_on():
+	magic_def_bonus = 1.5
+	set_magic_def()
+
+func set_defense_penalty_on():
+	defense_penalty = 0.5
+	set_defense()
+
+func set_strength_penalty_on():
+	strength_penalty = 0.5
+	set_strength()
+
+func set_magic_penalty_on():
+	magic_penalty = 0.5
+	set_magic()
+
+func set_magic_def_penalty_on():
+	magic_def_penalty = 0.5
+	set_magic_def()
 #####
 # Defense = ((base_def + equipment def) +/* positive_modifiers) -// negative_modifiers)
 #####
