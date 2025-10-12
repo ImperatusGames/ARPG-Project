c2e0292a66ea50f8c007ae3503221a0b00b9ef1b
lavoier <robflavoie@gmail.com>
2025-10-12 15:04:33 -0400

    Fix drop component error - unused velocity component
    Adding check for enemy having velocity component prior to using velocity for item drop

--- Components/drop_component.gd
+++ Components/drop_component.gd
@@ -9,7 +9,7 @@ class_name DropComponent
 #not all block variables included
 var can_move := true
 var direction = Vector2(.5,.5)
-@onready var velocity_component: VelocityComponent = $VelocityComponent
+#@onready var velocity_component: VelocityComponent = $VelocityComponent
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
@@ -23,12 +23,12 @@ func trigger_drop() -> void:
 		return
 	print("DROP")
 	var pickup = pickup_scene.instantiate()
-	pickup.item_resource = load("res://Resources/gold.tres") #item_resource ##TODO learn why setting export as gold causes issues
+	pickup.item_resource = item_resource #item_resource ##TODO learn why setting export as gold causes issues
 	pickup.global_position = get_parent().global_position
 	
 	var enemy = get_parent()
 	var inherited_velocity: Vector2 = Vector2(15.0,15.0)
-	if enemy is CharacterBody2D && enemy.velocity != Vector2(0,0):
+	if owner.has_node("VelocityComponent"): #enemy is CharacterBody2D && enemy.velocity != Vector2(0,0):
 		inherited_velocity = enemy.velocity
 	print(inherited_velocity)
 	inherited_velocity = inherited_velocity.rotated( randf_range(-1.5, 1.5) ) * randf_range(10 , 20 )
