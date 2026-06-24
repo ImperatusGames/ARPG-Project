5ddc552da9cc6e2fec2a88e6280bb391e80814ee
lavoier <robflavoie@gmail.com>
2026-06-23 21:11:19 -0400

    Addition of Mana Potion, restore MP
    Consumable ENUM health, MP, full restore
    MP Restore field and value
    Option for Full Restore

--- Resources/items/consumable_item.gd
+++ Resources/items/consumable_item.gd
@@ -1,7 +1,9 @@
 extends Item
 class_name ConsumableItem
 
-@export var heal_amount: int = 20
+@export_enum("Health", "MP", "Full Restore") var restore_target: String = "Health"
+@export var heal_amount: int = 0
+@export var mp_restore_amount: int = 0
 @export var effect_duration: float = 0.0  # For buffs like speed boost
 
 func _init(p_name: String = "", p_description: String = "", p_icon: Texture2D = null, p_value: int = 0, p_type: String = "Consumable", p_max_stack: int = 99, p_heal_amount: int = 20, p_effect_duration: float = 0.0):
@@ -10,8 +12,24 @@ func _init(p_name: String = "", p_description: String = "", p_icon: Texture2D = 
 	effect_duration = p_effect_duration
 
 func use(player: Node) -> void:
+	match restore_target:
+		"Health":
+			restore_health(player)
+		"MP":
+			restore_mp(player)
+		"Full Restore":
+			##TODO: Determine max health and mana to restore
+			restore_health(player)
+			restore_mp(player)
+
+func restore_health(player: Node) -> void:
 	if player.health_component.has_method("restore_health"):
 		Heal.heal_power = heal_amount
 		Heal.is_spell_heal = false
 		player.health_component.restore_health(Heal)
 	print(name, " used: Healed ", heal_amount, " HP")
+
+func restore_mp(player: Node) -> void:
+	if player.stats_component.has_method("restore_mp"):
+		player.stats_component.restore_mp(mp_restore_amount)
+		print(name, " used: Restored ", mp_restore_amount, " MP")
